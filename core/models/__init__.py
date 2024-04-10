6b77d02e52d90302444fea13bbb351a10f77bf52
Nitesh Yadav <nitesh.y@zifornd.com>
2024-04-10 14:47:12 +0530

    _

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -2,35 +2,39 @@ import torch
 import torch.nn as nn
 
 from core.config import device
-from core.components.cnn import ConvLayer
+from core.components.cnn import BasicBlock, BottleNeck, ResNet
 from core.components.embedding import TokenEmbedding
 from core.components.transformer import Encoder, Decoder
 
 
-class ConvNet(nn.Module):
-    def __init__(self, final_channels, ep=1e-7):
-        super().__init__()
-        self.epsilon = ep
-        self.conv1 = ConvLayer(1, 16, 4)
-        self.conv2 = ConvLayer(16, 32, 3)
-        self.conv3 = ConvLayer(32, 64, 2)
-        self.conv4 = ConvLayer(64, 128, 2, stride=2)
-        self.conv5 = ConvLayer(128, 256, 2, pool=2)
-        self.conv6 = ConvLayer(256, final_channels, 2, stride=2)
+def resnet18(in_channels=1, num_classes=100):
+    return ResNet(
+        BasicBlock, [2, 2, 2, 2], in_chanels=in_channels, num_classes=num_classes
+    )
 
-    def forward(self, x):
-        # normalize pixel values
-        mean = torch.mean(x, dim=(-1, -2), keepdim=True)
-        std = torch.std(x, dim=(-1, -2), keepdim=True)
-        x = (x - mean) / (std + self.epsilon)
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
-        x = self.conv6(x)
-        # mean operation will output (BATCH_SIZE, EMBEDDING_SIZE), does it make sense?
-        return x.mean(dim=(-1, -2))
+
+def resnet34(in_channels=1, num_classes=100):
+    return ResNet(
+        BasicBlock, [3, 4, 6, 3], in_chanels=in_channels, num_classes=num_classes
+    )
+
+
+def resnet50(in_channels=1, num_classes=100):
+    return ResNet(
+        BottleNeck, [3, 4, 6, 3], in_chanels=in_channels, num_classes=num_classes
+    )
+
+
+def resnet101(in_channels=1, num_classes=100):
+    return ResNet(
+        BottleNeck, [3, 4, 23, 3], in_chanels=in_channels, num_classes=num_classes
+    )
+
+
+def resnet152(in_channels=1, num_classes=100):
+    return ResNet(
+        BottleNeck, [3, 8, 36, 3], in_chanels=in_channels, num_classes=num_classes
+    )
 
 
 class Transformer(nn.Module):
